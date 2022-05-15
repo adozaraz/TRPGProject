922866eeb23339b331d1a8459ee27a76a72aac0b
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-05-15 21:23:56 +0400

    Первоначальная настройка экрана персонажей

--- frontend/src/app/components/characterListEditor/modal-character.ts
+++ frontend/src/app/components/characterListEditor/modal-character.ts
@@ -3,6 +3,7 @@ import {UserService} from "../../services/user.service";
 import {TokenStorageService} from "../../security/token-storage.service";
 import {Router} from "@angular/router";
 import {Role} from "../../models/user.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'modal-component',
@@ -10,4 +11,38 @@ import {Role} from "../../models/user.model";
     styleUrls: ['./modal-character.scss']
 })
 export class ModalCharacterComponent {
+    //characterList: FormGroup;
+    stats: FormGroup;
+    skillsProf: FormGroup;
+    savingThrowsProf: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        // this.characterList = this.formBuilder.group();
+        this.stats = this.formBuilder.group({
+            strength: "",
+            dexterity: "",
+            constitution: "",
+            intelligence: "",
+            wisdom: "",
+            charisma: ""
+        });
+        this.skillsProf = this.formBuilder.group({
+
+        });
+        this.savingThrowsProf = this.formBuilder.group({
+            strength: false,
+            dexterity: false,
+            constitution: false,
+            intelligence: false,
+            wisdom: false,
+            charisma: false
+        });
+    }
+
+    calculateModificator(number: number) {
+        // @ts-ignore
+        if (number != "") return Math.floor((number - 10) / 2);
+        else return " ";
+    }
+
 }
